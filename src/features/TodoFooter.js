b732bc66e642e47fefc3f9c43e510223bd4ad387
Mesrop Abazyan <107545114+MesropAbazyan@users.noreply.github.com>
2023-03-02 02:50:45 +0400

    Update TodoFooter.js

--- src/features/TodoFooter.js
+++ src/features/TodoFooter.js
@@ -1,21 +1,28 @@
 import './TodoFooter.css';
 
 export default function TodoFooter({todos, onClearCompleted}) {
-    const completed = todos.filter((todo) => todo.isCompleted).length;
+  const completed = todos.filter((todo) => todo.isCompleted).length;
 
-    return (
-        <div className='TodoFooter'>
-            <div><span>{completed}/{todos.length} completed</span></div>
-            <button onClick={onClearCompleted}>Clear completed</button>
-        </div>
-    );
+  return (
+    <div className='TodoFooter'>
+      <div>
+        <span>
+          <b>
+            <span className='done'>{completed}</span>/
+            <span className='all'>{todos.length}</span> completed
+          </b>
+        </span>
+      </div>
+      <button onClick={onClearCompleted}>Clear completed</button>
+    </div>
+  );
 }
 
 export function clearCompleted(clComp) {
-    return {
-        type: 'isCompleted',
-        payload: {
-          isCompleted: clComp.isCompleted,
-        },
-    };
+  return {
+    type: 'isCompleted',
+    payload: {
+      isCompleted: clComp.isCompleted,
+    },
+  };
 }
